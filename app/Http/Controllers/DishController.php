09936446a444483c12d3ee83e1aaf66f32343860
rastakorps <jose.oakenfold@gmail.com>
2024-01-29 22:11:20 -0600

    first commit

---
+++ app/Http/Controllers/DishController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dish;
+use Illuminate\Http\Request;
+
+class DishController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Dish $dish)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Dish $dish)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Dish $dish)
+    {
+        //
+    }
+}
